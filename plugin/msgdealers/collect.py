45e7f01a60d277c2ee4e08859cf491aa780667ac
xzjs <2310976219@qq.com>
2016-07-13 16:04:14 +0800

    测试中文乱码

--- plugin/msgdealers/collect.py
+++ plugin/msgdealers/collect.py
@@ -2,10 +2,8 @@
 
 
 def collect(msg, storageClass=None, userName=None):
-    # str = '智能网络研究所个人工作汇报'
-    # str = repr(str)
-    # if str not in msg:
-    #     return False
-    # storageClass.store_report(userName, msg)
-    # return False
-    return msg
+    str = '201'
+    if str not in msg:
+        return False
+    storageClass.store_report(userName, msg)
+    return False
