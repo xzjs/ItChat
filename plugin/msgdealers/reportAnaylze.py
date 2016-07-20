57866a082654f78bf47b1bbe454d65808ac97597
xzjs <2310976219@qq.com>
2016-07-20 13:25:40 +0800

    根据新的规则修复原先的获取生成语句

--- plugin/msgdealers/reportAnaylze.py
+++ plugin/msgdealers/reportAnaylze.py
@@ -6,61 +6,6 @@ from itchat import config
 from plugin.Sqlite3Client import Sqlite3Client
 
 
-def collect(msg, storageClass=None, userName=None):
-    str = '201'
-    if str in msg:
-        storageClass.store_report(userName, msg)
-    str == u'report'
-    if str in msg:
-        a = analyse()
-        text = a.get_sentence()
-        return text
-    if int(msg) > 1 and int(msg) < 9999999:
-        a = analyse()
-        text = a.liu(msg)
-        return text
-    return False
-
-
-class analyse:
-    def get_dict(self, names=None):
-        """ 将数据库中的语句生成字典 """
-        dict = {}
-        for name in names:
-            with Sqlite3Client('storage/account/血之君殇.db') as s3c:
-                # todo:这里应该搜索时间为今天的汇报
-                messages = s3c.query('select * from report WHERE UserName =? order by time desc', (name,))
-                if len(messages):
-                    messagelist = (messages[0][1]).split('<br/>')[1:]
-                    dict[name] = []
-                    for m in messagelist:
-                        if m != '':
-                            m_list = list(m)
-                            temp = []
-                            for word in m_list:
-                                if word not in u'(123456789). ）（。':
-                                    temp.append(word)
-                            dict[name].append(''.join(temp))
-        return dict
-
-
-class report:
-    def __init__(self, s, nickname, client, time_str='220000'):
-        self.s = s
-        self.nickname = nickname
-        self.client = client
-        self.time_str = time_str
-        self.username = self.s.find_user(self.nickname)
-
-    def do(self):
-        time_str = time.strftime('%H%M%S')
-        print time_str
-        if time_str == self.time_str:
-            a = analyse()
-            msg = a.get_sentence()
-            self.client.send_msg(self.username, msg)
-
-
 class reportAnaylze:
     def __init__(self):
         self.str = u'网络所(' + time.strftime('%Y%m%d') + u')网络所工作汇报:平行网络和车联网相关研究推进;'
@@ -97,29 +42,16 @@ class reportAnaylze:
             with Sqlite3Client(self.sqlDir) as s3c:
                 messages = s3c.query('select * from report WHERE UserName =? order by time desc', (name,))
                 if len(messages):
-                    messagelist = (messages[0][1]).split('<br/>')[1:]
-                    dict[name] = []
-                    for m in messagelist:
-                        if m != '':
-                            m_list = list(m)
-                            temp = []
-                            for word in m_list:
-                                if word not in u'(123456789). ）（。':
-                                    temp.append(word)
-                            dict[name].append(''.join(temp))
+                    messagelist = (messages[0][1])[4:]
+                    dict[name] = messagelist
         return dict
 
     def get_sentence(self):
         """ 获得最后的拼装语句 """
         for i in range(0, len(self.names)):
             for value in self.get_dict(self.names[i]).values():
-                for sentence in value:
-                    if sentence != '':
-                        self.strs[i] += sentence + ','
+                self.strs[i] += value + ','
         self.str += ''.join(self.strs)
-        l = list(self.str)
-        l[-1] = '.'
-        self.str = ''.join(l)
         return self.str
 
     def liu(self):
@@ -127,5 +59,5 @@ class reportAnaylze:
         dict = self.get_dict(user_list)
         str = ''
         for key in dict.keys():
-            str += key + ':' + ','.join(dict[key]) + '\n'
+            str += key + ':' + dict[key] + '\n'
         return str
